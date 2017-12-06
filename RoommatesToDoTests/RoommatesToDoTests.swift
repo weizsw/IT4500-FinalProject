5243d872114f8d0831d5f7c8872a6a57ad5c0f06
anson <anson@zoeker.cn>
2017-12-06 17:11:09 +0800

    Initial Commit

---
+++ RoommatesToDoTests/RoommatesToDoTests.swift
@@ -0,0 +1,36 @@
+//
+//  RoommatesToDoTests.swift
+//  RoommatesToDoTests
+//
+//  Created by Anson on 2017/12/6.
+//  Copyright © 2017年 Anson. All rights reserved.
+//
+
+import XCTest
+@testable import RoommatesToDo
+
+class RoommatesToDoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
